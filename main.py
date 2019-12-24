c805b6aa4542208ca2c5c005ddf7c84af19d85de
Jacob Bruinsma <me@jacobbruinsma.com>
2019-12-24 12:23:59 -0600

    better status bar, better maybePrint

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ urlToFilename = lambda url: re.search(r'/([^/]*?\.mp4)', url).group(1)
 isPlayList = lambda url: '.urlset' in url
 toList = lambda lst: [u for u in re.split(r'#EXT.*?,', lst) if len(u)]
 askOpen = lambda fileName: 'y' == input(f'{fileName} DONE. Open? y/n> ')
-maybePrint = lambda future, text, end: print(text, end=end, flush=True) if future.done() else None
-def getPlayList(printWhenReady, url):
-	printWhenReady('Downloading parts list...', end='\n')
+maybePrint = lambda future, text, end='\n': print(text, end=end, flush=True) if future.done() else None
+def getPlayList(printIfReady, url):
+	printIfReady('Downloading parts list...')
 	return requests.get(url).text.replace('\n','').replace('#EXT-X-ENDLIST','')
-def appendPart(out, num, url, printWhenReady):
+def appendPart(out, num, url, printIfReady):
 	retry = True
 	while retry:
 		downloaded=0
@@ -32,13 +32,15 @@ def appendPart(out, num, url, printWhenReady):
 			for data in response.iter_content(1024*1024):
 				downloaded+=len(data)
 				out.write(data)
-				printWhenReady(f'{100*downloaded//totalLength*"#":.<100} {downloaded: >13,} B of {totalLength:,} B   {len(data)/(time()-last): >10,.0f} B/s{" "*20}', end='\r')
+				pb = f'{100*downloaded//totalLength*"#":.<100} {downloaded: >13,} B of {totalLength:,} B   {len(data)/(time()-last): >10,.0f} B/s{" "*20}'
+				pb = '`'.join([pb[(x-1)*10:x*10] for x in range(1, 11)]) + pb[100:]
+				printIfReady(pb, end='\r')
 				last = time()
-				printWhenReady(f'Finished {downloaded:,} B.')
+			printIfReady(f'\nFinished {downloaded:,} B.')
 			break
 		except Exception as exc:
 			retry = 'y' == input(f'This part failed to download ({exc}). Try again? y/n')
-			printWhenReady(f'Downloaded {downloaded:,} B, {"retrying..." if retry == "y" else "gave up."}')
+			printIfReady(f'Downloaded {downloaded:,} B, {"retrying..." if retry == "y" else "gave up."}')
 
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -48,14 +50,14 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
 	tempName = urlToFilename(pastedUrl)
 	print(f'{prefix} --> {tempName}...')
 	nameInputter = executor.submit(askFilename)
-	printWhenReady = partial(maybePrint, nameInputter)
-	parts = toList(getPlayList(printWhenReady, pastedUrl)) if isPlayList(pastedUrl) else [pastedUrl] 
+	printIfReady = partial(maybePrint, nameInputter)
+	parts = toList(getPlayList(printIfReady, pastedUrl)) if isPlayList(pastedUrl) else [pastedUrl] 
 
 	with open(tempName, 'wb') as out:
 		for num, part in enumerate(parts, start=1):
-			printWhenReady(f'Downloading {num: >3} of {len(parts)}: {part[:30]}... ', end=None)
+			printIfReady(f'Downloading {num: >3} of {len(parts)}: {part[:30]}... ', end=None)
 			url = prefix+'/'+part if isPlayList(pastedUrl) else part
-			appendPart(out, num, url, printWhenReady)
+			appendPart(out, num, url, printIfReady)
 
 	newName = nameInputter.result() + ' ' + tempName
 	os.rename(tempName, newName)

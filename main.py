dd12be404b8ac99a257a63e7c8f96969acc6b3e4
Jacob Bruinsma <me@jacobbruinsma.com>
2019-12-24 08:26:38 -0600

    M main.py, async input, better printing.

--- main.py
+++ main.py
@@ -3,54 +3,61 @@ import re
 import requests
 import subprocess
 from time import time
-import threading
+import concurrent.futures
+import os
+from functools import partial
 
 # ...xxx.mp4.urlset/index-f1-v1-a1.m3u8
 # or:
 # ...xxx.mp4?val...
-askUrl = lambda: input('Paste url > ')
-askFilename = lambda: input('Save as  > ')
+askUrl = lambda: input('Paste url> ')
+askFilename = lambda: input('Save with prefix> ')
 urlPathPrefix = lambda url: url.rpartition('/')[0]
-formatFilename = lambda saveAs, partsListUrl: saveAs + re.search(r'/([^/]*?\.mp4)', partsListUrl).group(1)
+urlToFilename = lambda url: re.search(r'/([^/]*?\.mp4)', url).group(1)
 isPlayList = lambda url: '.urlset' in url
 toList = lambda lst: [u for u in re.split(r'#EXT.*?,', lst) if len(u)]
-askOpen = lambda: 'y' == input(f'{fileName} DONE. Open? y/n > ')
-def getPlayList(url):
-	print('Downloading parts list...')
+askOpen = lambda fileName: 'y' == input(f'{fileName} DONE. Open? y/n> ')
+maybePrint = lambda future, text, end: print(text, end=end, flush=True) if future.done() else None
+def getPlayList(printWhenReady, url):
+	printWhenReady('Downloading parts list...', end='\n')
 	return requests.get(url).text.replace('\n','').replace('#EXT-X-ENDLIST','')
-def appendPart(out, num, url):
+def appendPart(out, num, url, printWhenReady):
 	retry = True
 	while retry:
+		downloaded=0
 		try:
 			response = requests.get(url, stream=True)
 			totalLength = int(response.headers.get('content-length'))
-			print(f'part size: {totalLength:,} B')
-			downloaded=0
-			print(100*'.', end='\r', flush=True)
 			last = time()
 			for data in response.iter_content(1024*1024):
 				downloaded+=len(data)
 				out.write(data)
-				print(f'{100*downloaded//totalLength*"#":.<100} {downloaded: >13,} B   {len(data)/(time()-last): >10,.0f} B/s{" "*20}', end='\r', flush=True)
+				printWhenReady(f'{100*downloaded//totalLength*"#":.<100} {downloaded: >13,} B of {totalLength:,} B   {len(data)/(time()-last): >10,.0f} B/s{" "*20}', end='\r')
 				last = time()
-			print(100*'#', flush=True)
+				printWhenReady(f'Finished {downloaded:,} B.')
 			break
 		except Exception as exc:
 			retry = 'y' == input(f'This part failed to download ({exc}). Try again? y/n')
+			printWhenReady(f'Downloaded {downloaded:,} B, {"retrying..." if retry == "y" else "gave up."}')
 
-print('Get the quality_1080p url from browser console.')
-pastedUrl = askUrl()
-saveAs = askFilename()
-prefix = urlPathPrefix(pastedUrl)
-fileName = formatFilename(saveAs, pastedUrl)
-print(f'{prefix} --> {fileName}...')
-parts = toList(getPlayList(pastedUrl)) if isPlayList(pastedUrl) else [pastedUrl] 
 
-with open(fileName, 'wb') as out:
-	for num, part in enumerate(parts, start=1):
-		print(f'Downloading {num: >3} of {len(parts)}: {part[:30]}... ', end='')
-		url = prefix+'/'+part if isPlayList(pastedUrl) else part
-		appendPart(out, num, url)
+with concurrent.futures.ThreadPoolExecutor() as executor:
+	print('Get the quality_1080p url from browser console.')
+	pastedUrl = askUrl()
+	prefix = urlPathPrefix(pastedUrl)
+	tempName = urlToFilename(pastedUrl)
+	print(f'{prefix} --> {tempName}...')
+	nameInputter = executor.submit(askFilename)
+	printWhenReady = partial(maybePrint, nameInputter)
+	parts = toList(getPlayList(printWhenReady, pastedUrl)) if isPlayList(pastedUrl) else [pastedUrl] 
 
-if askOpen():
-        subprocess.run(['explorer', fileName])
+	with open(tempName, 'wb') as out:
+		for num, part in enumerate(parts, start=1):
+			printWhenReady(f'Downloading {num: >3} of {len(parts)}: {part[:30]}... ', end=None)
+			url = prefix+'/'+part if isPlayList(pastedUrl) else part
+			appendPart(out, num, url, printWhenReady)
+
+	newName = nameInputter.result() + ' ' + tempName
+	os.rename(tempName, newName)
+	if askOpen(newName):
+		subprocess.run(['explorer', newName])

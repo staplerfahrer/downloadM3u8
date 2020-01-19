758e209adc9373f9fe816e9ddf0a7ba8cfb4b339
Jacob Bruinsma <me@jacobbruinsma.com>
2020-01-19 11:55:12 -0600

    M removed comment, removed title-reading from H1

--- getDlUrl.py
+++ getDlUrl.py
@@ -11,14 +11,13 @@ def urlAndTitle(pageUrl):
 	start = time()
 	driver = webdriver.Chrome('./bin/chromedriver.exe')
 	driver.set_window_size(100,100)
-	#driver.minimize_window()#this seems slower...
 	driver.get(pageUrl.partition('&')[0])
 	
 	qualityUrl = lambda variable: driver.execute_script(f'return typeof({variable}) !== "undefined" ? {variable} : ""') 
 	qualities = ['quality_1080p','quality_720p','quality_480p','quality_360p','quality_240p']
 	detected = [u for u in [qualityUrl(q) for q in qualities] if u]
-	fromH1 = driver.find_element_by_xpath('//h1[@class="title"]/span').text
-	title = ''.join(c for c in (fromH1 if fromH1 else driver.title).casefold() if c in 'abcdefghijklmnopqrstuvwxyz1234567890- ,')
+	#fromH1 = driver.find_element_by_xpath('//h1[@class="title"]/span').text
+	title = ''.join(c for c in (driver.title).casefold() if c in 'abcdefghijklmnopqrstuvwxyz1234567890- ,')#fromH1 if fromH1 else
 	driver.close()
 	print(f'Took {time()-start:.2f} s.')
 	return detected[0], title
